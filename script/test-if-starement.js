1e48aeb042b7b6898cac5599b4a79df7502a6758
sayfullah3278 <sayfullah3278@gmail.com>
2023-05-21 12:56:01 +0600

    asss

---
+++ script/test-if-starement.js
@@ -0,0 +1,55 @@
+//for(let i=1; i<=100; i++){
+  //  if(i%2==0){
+   //     console.log(i);
+  //  }
+ //   else{
+
+ //   }
+//}
+
+//for (let i=1; i<=100; i++){
+ //   if(i%2==0){
+    
+//    }
+ //   else{
+  //      console.log(i);
+    
+//}
+//}
+
+//for (let i=1; i<=50; i++){
+  //  if(i%3==0 || i%5==0){
+  //      console.log(i);
+  //  }
+
+    
+  //  else{
+
+  //  }
+//}
+
+//for (let i=1; i>=50; i++){
+   // if(i%3==0 || i%5==0){
+   //     console.log(i);
+   // }
+
+    
+  //  else{
+
+  //  }
+//}
+//5!
+//5*4!
+//5*4*3!
+//5*4*3*2!
+//5*4*3*2*1
+
+let inpute=5;
+let result=1;
+
+for(let i=inpute; i>0; i--){
+     result=result*i;
+     //result*=i;
+
+}
+console.log(result);
