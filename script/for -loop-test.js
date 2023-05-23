01ab3e66b2a71436a6b601990bc8f16ea7580f1f
sayfullah3278 <sayfullah3278@gmail.com>
2023-05-23 12:58:54 +0600

    add23.5.23

---
+++ script/for -loop-test.js
@@ -0,0 +1,113 @@
+//let userName = "jata";
+//let password = "dada";
+
+//if (userName == "java"){
+// console.log("welcome "+ userName);
+//if(password =="dada"){
+ //console.log("welcom "+ userName);
+//}
+//else{
+
+//}
+//}
+//else{
+//    console.log("u n not found");
+//}
+
+ //let A+ >= 80;
+ //let A >=70;
+ //let A- >=60;
+ //let B >=50;
+ //let C >=40;
+ //let D >=33
+ //let F >=32
+
+ //let marks=-50;
+  
+ //if(marks<0){
+ //   console.log("Invalid number")
+
+ //}
+ //else if (marks<=32){
+  //  console.log("F");
+ //}
+ //else if (marks<=39){
+//    console.log("D");
+ //}
+ //else if (marks<=49){
+ //   console.log("C");
+ //}
+ //else if (marks<=59){
+ //   console.log("B");
+ //}
+ //else if (marks<=69){
+ //   console.log("A-");
+ //}
+ //else if (marks<=79){
+ //   console.log("A");
+ //}
+ //else if (marks<=100){
+ //   console.log("A+");
+// }
+ //else{
+ //   console.log("Invalid number");
+// }
+
+
+//let day=8;
+//
+//switch(day){
+//    case 1:
+//        console.log("sat");
+//        break;
+//    case 2:
+//        console.log("sut");
+//        break;
+//    case 3:
+//        console.log("mon");
+//        break;
+//    case 4:
+//        console.log("twis");
+//        break;
+//    case 5:
+//        console.log("wins");
+//        break;
+//    case 6:
+//        console.log("thus");
+//        break;
+//    case 7:
+//        console.log("fri");
+//        break;
+//    default:
+//        console.log("Nothing found")    
+//}
+
+
+//let number=9;
+//let counter=0;
+//
+//for(let i=1; i<=number; i++){
+//    if(number%i==0){
+//        counter=counter+1;
+//    }
+//}
+//if(counter==2){
+//    console.log("prime");
+//}
+//else{
+//    console.log("not prime");
+//}
+
+let n1=0;
+let n2=1;
+let sum=0;
+
+console.log(n1);
+console.log(n2);
+
+for(i=3; i<=8; i++){
+    sum=n1+n2;
+    console.log(sum);
+    n1=n2;
+    n2=sum;
+}
---
+++ script/omit numder.js
@@ -0,0 +1,26 @@
+let n=1;
+
+for(let i=n; i<=50; i++){
+    if(n%3==0 && n%5==0){
+       
+    }
+    else{
+        console.log(n);
+        
+    }
+    n++;
+}
+
+//let n=1;
+//
+//while(n<=50){
+//    if(n%3==0 && n%5==0){
+//        n++;
+//        continue;
+//    }
+//    else{
+//        console.log(n);
+//    }
+//        n++;
+//
+//}
