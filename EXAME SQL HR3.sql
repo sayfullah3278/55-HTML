9b9d792319bcbfb14d1b2ae55b8ac5c8f556e25f
sayfullah3278 <sayfullah3278@gmail.com>
2023-07-09 12:29:46 +0600

    9.07.23ADD
    SQL WORK

--- EXAME SQL HR3.sql
+++ EXAME SQL HR3.sql
@@ -45,5 +45,8 @@ select d.department_name,  Avg(e.salary),count(e.employee_id)
 from employees e
 join departments d on e.department_id = d.department_id
 where e.commission_pct is not null
-group by d.department_name;                
+group by d.department_name;     
+
+
+commit;
 
