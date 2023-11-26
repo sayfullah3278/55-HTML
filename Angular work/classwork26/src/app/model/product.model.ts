17e1c4ad2f5e4cc0fbf9301829b8961708870728
sayfullah3278 <sayfullah3278@gmail.com>
2023-11-26 12:35:16 +0600

    addwork26
    addwork26nov

---
+++ Angular work/classwork26/src/app/model/product.model.ts
@@ -0,0 +1,9 @@
+export class Product{
+    constructor(
+     public   id ?: number,
+    public name ?: string,
+   public catagory ?: string,
+  public  description ?: string,
+   public price ?: number,
+    ){}
+}
